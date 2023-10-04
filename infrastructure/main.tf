c4e44df95ecd6e222f029052d0d9acfa1c0779a2
Malthe Poulsen <malthe@grundtvigsvej.dk>
2023-10-04 21:16:06 +0200

    Issue should be fixed now

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -88,10 +88,14 @@ resource "azurerm_static_site" "static_site" {
   sku_size            = "Free"
 }
 
+resource "github_actions_secret" "update_function_app_name_secret" {
+  repository      = "automatic-deployment-workflow"
+  secret_name     = "${var.environment}_function_app_name"
+  plaintext_value = azurerm_linux_function_app.function_app.name
+}
+
 resource "github_actions_secret" "update_site_api_key_secret" {
   repository      = "automatic-deployment-workflow"
   secret_name     = "${var.environment}_api_key"
   plaintext_value = azurerm_static_site.static_site.api_key
 }
-
-#
